a03c00dad14c54eb93fff51a2dfb611b84ba9094
David Rocamora <david.rocamora@controlgroup.com>
2013-11-16 10:32:59 -0500

    Comment and README

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my attempt to make a venti server that stores to s3. It is super innefficient now.
--- s3venti.go
+++ s3venti.go
@@ -1,6 +1,4 @@
-// Copyright 2010 The Govt Authors.  All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// TODO: Add license and show some attribution to govt
 
 package main
 
